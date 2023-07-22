b022bcf3a9c7e64a024c7f040671bdd54a58989e
Andrew <odoleandrew@yahoo.com>
2023-07-22 12:35:39 +0100

    first commit

---
+++ contracts/gasChallenge.sol
@@ -0,0 +1,40 @@
+//using caching tends to increase the gas fees
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract gasChallenge {
+    //Implement Fixed-Size Array Technique Here
+    uint[10] numbers = [1,2,3,4,5,6,7,8,9,10];
+    
+    
+    //Function to check for sum of array
+    //No changes required in this function
+    function getSumOfArray() public view returns (uint256) {
+        uint sum = 0;
+        for (uint i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+    
+    function notOptimizedFunction() public {
+        for (uint i = 0; i < numbers.length; i++) {
+            numbers[i] = 0;
+        }
+    }
+    
+    //Implement Remaining Gas Optimization Techniques Here
+    //Sum of elements in the numbers array should equal 0
+    function optimizedFunction() public {
+    
+    unchecked {
+        uint[10] memory newnumbers = numbers;
+       
+        for (uint i = 0; i < 10 ; i +=1) {
+           cachednumbers[i] = 0;
+        }
+        numbers = newnumbers;
+    }
+    }
+
+ }
